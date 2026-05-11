deae57b408daa8a77fc855780d96303a9fe7c4df
Pedro Henrique Baliza Ramos <pedro.bramos@sempreceub.com>
2026-05-11 20:37:33 -0300

    Refactor height input and pyramid printing logic
    Fix indentation and add height input validation.

--- mario.py
+++ mario.py
@@ -1,6 +1,19 @@
 from cs50 import get_int
-
+#import height
 while True:
-  answer = get_int("Digit a number greater than 1 and smaller than 8")
-  break
+    height = get_int("Digit a number greater than 1 and smaller than 8: ")
+    if  1 <= height <= 8
+         break
+
+for i in range (height):
+     #spaces
+    for s in range (height - i - 1):
+      print(" ",end="")
+     #print #
+    for a in range (i + 1):
+      print("#", end="")
+
+    print()
+
+      
   
