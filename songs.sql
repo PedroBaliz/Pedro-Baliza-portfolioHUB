14fd386023a1b4855dfabf27530ddb00c67627fe
Pedro Henrique Baliza Ramos <pedro.bramos@sempreceub.com>
2026-05-19 21:35:34 -0300

    Add new SQL queries for song analysis

--- songs.sql
+++ songs.sql
@@ -11,12 +11,15 @@ FROM songs
 ORDER BY duration_ms DESC
 LIMIT 5;
 
+
 SELECT name
 FROM songs
 WHERE danceability > 0.75 AND energy > 0.75 AND valence > 0.75;
 
+
 SELECT AVG(energy)
 FROM songs;
+
 
 SELECT name
 From songs
@@ -27,10 +30,12 @@ FROM artists
 WHERE name = 'Post Malone'
 );
 
+
 SELECT AVG(energy)
 FROM songs
 JOIN artists ON songs.artist_id = artists.id
 WHERE artists.name = 'Drake';
+
 
 SELECT name
 FROM songs
